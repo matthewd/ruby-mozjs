5e55a9ccad85aaa8db6c4a995cd7a3bc99e6df86
Matthew Draper <matthew@trebex.net>
2010-05-14 16:56:30 +0930

    Simplify extconf.rb -- this change might break Windows support?

--- extconf.rb
+++ extconf.rb
@@ -1,33 +1,13 @@
 require 'mkmf'
 require 'pkg-config'
 
-def find_smjs(mozjs)
-  dir_config(mozjs)
-  #$CFLAGS += " -gdbg"
-  case CONFIG['target_os']
-  when /mswin32|mingw|bccwin32/
-    $defs << " -DXP_WIN"
-    lib = "js32"
-  else
-    $defs << " -DXP_UNIX"
-    lib = mozjs
-  end
-
-  $defs << " -DNEED_#{mozjs.upcase}_PREFIX"
-  have_library(lib)
-end
-
 $defs << " -Wall"
 
-if find_smjs('mozjs') or (CONFIG['target_os'] =~ /mswin32|mingw|bccwin32/ and (find_smjs('mozjs') or find_smjs('smjs'))) or
-  %w(xulrunner-js thunderbird-js mozilla-js).any? do |package|
-    PKGConfig.have_package(package)
-  end
-
-  have_func("JS_SetOperationCallback")
-  have_func("JS_SetBranchCallback")
-
-  create_makefile("spidermonkey")
-else
-  exit 1
+%w(xulrunner-js thunderbird-js mozilla-js).any? do |package|
+  PKGConfig.have_package(package)
 end
+
+have_func("JS_SetOperationCallback")
+have_func("JS_SetBranchCallback")
+
+create_makefile("spidermonkey")
